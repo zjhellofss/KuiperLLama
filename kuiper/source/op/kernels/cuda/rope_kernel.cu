d5865964bcddfafabee2ee9aef0c9714c32b91d6
fss <hellofss@foxmail.com>
2024-07-09 23:54:31 +0800

    修改rope实现，删除无用的核函数，修改Matmul中的参数命名

--- kuiper/source/op/kernels/cuda/rope_kernel.cu
+++ kuiper/source/op/kernels/cuda/rope_kernel.cu
@@ -1,5 +1,13 @@
 #include "rope_kernel.cuh"
 namespace kernel {
+
+__device__ void rope_calc(float fcr, float fci, float* vec, int32_t idx) {
+  float v0 = vec[idx];
+  float v1 = vec[idx + 1];
+  vec[idx] = v0 * fcr - v1 * fci;
+  vec[idx + 1] = v0 * fci + v1 * fcr;
+}
+
 __global__ void rope_kernel_cu_fp32(int pos, int dim, int kv_dim, int head_size,
                                     const float* input_q, const float* input_k) {
   int idx = threadIdx.x + blockDim.x * blockIdx.x;
@@ -9,25 +17,19 @@ __global__ void rope_kernel_cu_fp32(int pos, int dim, int kv_dim, int head_size,
   }
 
   int head_dim = idx % head_size;
-  float freq =
-      1.0f / pow(10000.0f, static_cast<float>(head_dim) / static_cast<float>(head_size));
+  float freq = 1.0f / pow(10000.0f, static_cast<float>(head_dim) / static_cast<float>(head_size));
   float val = static_cast<float>(pos) * freq;
   float fcr = cosf(val);
   float fci = sinf(val);
-  int rotn = idx < kv_dim ? 2 : 1;
-#pragma unroll
-  for (int v = 0; v < rotn; v++) {
-    float* vec = const_cast<float*>(v == 0 ? input_q : input_k);
-    float v0 = vec[idx];
-    float v1 = vec[idx + 1];
-    vec[idx] = v0 * fcr - v1 * fci;
-    vec[idx + 1] = v0 * fci + v1 * fcr;
+  rope_calc(fcr, fci, const_cast<float*>(input_q), idx);
+  if (idx >= kv_dim) {
+    return;
   }
+  rope_calc(fcr, fci, const_cast<float*>(input_k), idx);
 }
 
-void rope_kernel_cu(int32_t dim, int32_t kv_dim, int32_t head_size,
-                    const tensor::Tensor& input_q, const tensor::Tensor& input_k,
-                    const tensor::Tensor& input_pos, void* stream) {
+void rope_kernel_cu(int32_t dim, int32_t kv_dim, int32_t head_size, const tensor::Tensor& input_q,
+                    const tensor::Tensor& input_k, const tensor::Tensor& input_pos, void* stream) {
   const int32_t pos = *input_pos.ptr<int32_t>(0);
   int threads = 512;
   int blocks = (dim + threads - 1) / threads;
@@ -36,8 +38,8 @@ void rope_kernel_cu(int32_t dim, int32_t kv_dim, int32_t head_size,
     rope_kernel_cu_fp32<<<blocks, threads, 0, stream_>>>(
         pos, dim, kv_dim, head_size, input_q.ptr<float>(), input_k.ptr<float>());
   } else {
-    rope_kernel_cu_fp32<<<blocks, threads>>>(pos, dim, kv_dim, head_size,
-                                             input_q.ptr<float>(), input_k.ptr<float>());
+    rope_kernel_cu_fp32<<<blocks, threads>>>(pos, dim, kv_dim, head_size, input_q.ptr<float>(),
+                                             input_k.ptr<float>());
   }
 }
 }  // namespace kernel
--- kuiper/source/op/kernels/cuda/scale_kernel.cuh
+++
@@ -1,7 +0,0 @@
-#ifndef SCALE_KERNEL_CU_H
-#define SCALE_KERNEL_CU_H
-#include <tensor/tensor.h>
-namespace kernel {
-void scale_inplace_cu(float scale, const tensor::Tensor& tensor, void* stream = nullptr);
-}
-#endif //SCALE_KERNEL_CU_H
