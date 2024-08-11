a38bb4adc89c91711f4f81e185ed388c4a6215ed
fss <hellofss@foxmail.com>
2024-08-11 12:58:40 +0800

    增加单元测试

--- test/test_op/test_cu_matmul.cpp
+++ test/test_op/test_cu_matmul.cpp
@@ -44,3 +44,33 @@ TEST(test_matmul_cu, matmul_linear_stream5) {
     ASSERT_EQ(out_cu.index<float>(i), out_cpu.index<float>(i));
   }
 }
+
+TEST(test_matmul_cu, matmul_linear_course) {
+  auto alloc_cu = base::CUDADeviceAllocatorFactory::get_instance();
+  auto alloc_cpu = base::CPUDeviceAllocatorFactory::get_instance();
+
+  tensor::Tensor input(base::DataType::kDataTypeFp32, 3, true, alloc_cpu);
+  tensor::Tensor weight(base::DataType::kDataTypeFp32, 3, 3, true, alloc_cpu);
+
+  input.index<float>(0) = float(1);
+  input.index<float>(1) = float(1);
+  input.index<float>(2) = float(-1);
+
+  for (int i = 1; i <= 9; ++i) {
+    weight.index<float>(i - 1) = float(i);
+  }
+  tensor::Tensor input_cpu = input.clone();
+  tensor::Tensor weight_cpu = weight.clone();
+
+  input.to_cuda(nullptr);
+  weight.to_cuda(nullptr);
+
+  tensor::Tensor out_cpu(base::DataType::kDataTypeFp32, 3, true, alloc_cpu);
+
+  kernel::get_matmul_kernel(base::DeviceType::kDeviceCPU)(input_cpu, weight_cpu, out_cpu, 1.f,
+                                                          nullptr);
+
+  ASSERT_EQ(out_cpu.index<float>(0), 0);
+  ASSERT_EQ(out_cpu.index<float>(1), 3);
+  ASSERT_EQ(out_cpu.index<float>(2), 6);
+}
